4677f2d1285400c1a0e5bae55114477d5ad1c25a
christian <chri75ky@stud.kea.dk>
2022-11-30 23:26:16 +0100

    Can post a review which will hopefully be sent to the database

---
+++ js/createReview.js
@@ -0,0 +1,22 @@
+'use strict'
+
+function addReview(){
+
+    let formData = {
+       amountOfStars: $("input[name='stars']:checked").val(),
+        reviewMessage: $("#besked-tekst").val()
+    };
+
+
+    $.ajax({
+    type: "POST",
+    url: "http://localhost:8080/api/reviews",
+    data: JSON.stringify(formData),
+    dataType: "json",
+    encode: true,
+    headers: {"Content-Type": "application/json;charset=UTF-8"}
+}).done(function(data) {
+console.log(data);
+});
+    event.preventDefault();
+}
